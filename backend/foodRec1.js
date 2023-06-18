80a7426e0599e140c1e8b7b85a93e5e6ad8d9ed6
Anurag-Raut <rautab0@gmail.com>
2023-06-18 23:12:10 +0530

    third commit

---
+++ backend/foodRec1.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api.js');
+const http = require('http');
+const express = require('express');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+const serviceNumber=1
+app.use(cors());
+
+const queue = 'food';
+const prefetch = 3;
+let count = 0;
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    
+    
+    
+
+    console.log(`[*] Waiting for messages in ${queue}. To exit, press CTRL+C`);
+
+    io.on('connection', (socket) => {
+      console.log('User connected');
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+      channel.prefetch(prefetch);
+      socket.on('disconnect', () => {
+        channel.deleteQueue(queue);
+        socket.disconnect()
+        console.log('User disconnected');
+      });
+
+      channel.consume(
+        queue,
+        function (msg) {
+          if (msg !== null) {
+            var a = Math.floor((Math.random() * 10 + 5) * 1000);
+           
+            // msg=msg.content.type=queue;
+            var message = msg.content.toString();
+            message=JSON.parse(message)
+            message.type=queue
+            message.time=a;
+            message.serviceNumber=serviceNumber
+            message=JSON.stringify(message)
+            console.log(message);
+            
+            socket.emit('received', message);
+            console.log(`Received at ${queue}, orderid: ${count++}: ${message}`);
+      
+            setTimeout(function () {
+              channel.ack(msg);
+              console.log('done');
+              socket.emit('done', message);
+            }, a);
+          }
+        },
+        { noAck: false }
+      );
+      
+    });
+  });
+});
+
+httpServer.listen(4000, () => {
+  console.log('foodRec is running on port 4000');
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import { recieverFoodSocket } from "../receiverFoodSocket";
+import { senderSocket } from "../senderSocket";
+import { recieverClothSocket } from "../receiverClothSocket";
+import Queue from "./componenets/queue";
+import Countdown from "react-countdown";
+import QueueCon from "./componenets/queueCon";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
+
+senderSocket.on("fullQueue", (data) => {
+  toast.warn('Queue Full', {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });
+  console.log("full queue");
+});
+
+
+function App() {
+  const [foodQueue, setFoodQueue] = useState([]);
+  const [clothQueue, setClothQueue] = useState([]);
+  const [foodCon1, setFoodCon1] = useState([]);
+  const [foodCon2, setFoodCon2] = useState([]);
+  const [clothCon, setClothCon] = useState([]);
+  const [count, setCount] = useState(0);
+
+  async function sendRequest(count, setCount) {
+    setCount(count + 1);
+    console.log(document.getElementById("select").value);
+    senderSocket.emit("send", {
+      type: document.getElementById("select").value,
+      orderId: count,
+    });
+    console.log({
+      type: document.getElementById("select").value,
+      orderId: count,
+    });
+  }
+
+  useEffect(() => {
+    const addedHandler = ({ orderId, type }) => {
+      if (type === "cloth") {
+        setClothQueue((prevQueue) => [...prevQueue, parseInt(orderId)]);
+      } else {
+        setFoodQueue((prevQueue) => [...prevQueue, parseInt(orderId)]);
+      }
+    };
+
+    const receivedHandler = (data) => {
+      data = JSON.parse(data);
+      console.log(data, "received");
+      if (data.type === "cloth") {
+        console.log("hi");
+        setClothQueue((prevQueue) =>
+          prevQueue.filter((value) => value !== data.orderId)
+        );
+        setClothCon((prevQueue) => [...prevQueue, data]);
+      } else {
+        setFoodQueue((prevQueue) =>
+          prevQueue.filter((value) => value !== data.orderId)
+        );
+
+        setFoodCon1((prevQueue) => [...prevQueue, data]);
+      }
+    };
+
+    const doneHandler = (data) => {
+      data = JSON.parse(data);
+      console.log(`done`, data);
+      if (data.type === "cloth") {
+        console.log("hi");
+        setClothCon((prevQueue) =>
+          prevQueue.filter((value) => value.orderId !== data.orderId)
+        );
+      } else {
+        setFoodCon1((prevQueue) =>
+          prevQueue.filter((value) => value.orderId !== data.orderId)
+        );
+      }
+    };
+
+    senderSocket.on("added", addedHandler);
+    recieverFoodSocket.on("received", receivedHandler);
+    recieverFoodSocket.on("done", doneHandler);
+    recieverClothSocket.on("received", receivedHandler);
+    recieverClothSocket.on("done", doneHandler);
+
+    return () => {
+      senderSocket.off("added", addedHandler);
+      recieverFoodSocket.off("received", receivedHandler);
+      recieverFoodSocket.off("done", doneHandler);
+      recieverClothSocket.off("received", receivedHandler);
+      recieverClothSocket.off("done", doneHandler);
+    };
+  }, []);
+
+  return (
+    <div className="flex justify-around items-center h-[90vh]">
+      <div className="card">
+        <label
+          for="countries"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Select an option
+        </label>
+        <select
+          id="select"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="food">Food</option>
+          <option value="cloth">clothing</option>
+        </select>
+        <button
+        className="mt-3"
+          onClick={() => {
+            sendRequest(count, setCount);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+      <div className="flex flex-col justify-between h-full">
+        <div className="h-[40vh] flex justify-between w-[40vw]">
+          <div>
+            <h4 className="">Food Queue</h4>
+            <Queue queue={foodQueue} />
+          </div>
+          <div>
+            <h4 className="">Food consumer</h4>
+            <QueueCon queue={foodCon1} />
+          </div>
+        </div>
+        <div className="h-[40vh] flex justify-between w-[40vw]">
+          <div>
+            <h4 className="">Clothes Queue</h4>
+            <Queue queue={clothQueue} />
+          </div>
+          <div>
+            <h4 className="">Clothes consumer</h4>
+            <QueueCon queue={clothCon} />
+          </div>
+        </div>
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+}
+
+export default App;
