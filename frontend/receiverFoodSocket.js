8107a070ed92aedbf5a772f5e8871f8c7b404cc3
Anurag-Raut <rautab0@gmail.com>
2023-06-19 18:56:23 +0530

    changed socket urls

--- frontend/receiverFoodSocket.js
+++ frontend/receiverFoodSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://157.245.109:4000'
+const URL = 'http://157.245.109:4000'
 
 export const recieverFoodSocket = io(URL);
--- frontend/senderSocket.js
+++ frontend/senderSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://157.245.109:3000';
+const URL = 'http://157.245.109:3000';
 
 export const senderSocket = io(URL);
