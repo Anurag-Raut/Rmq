d59f3a4f6ddf924ae770c6b3176dc4b8e0f4cbb4
Anurag-Raut <rautab0@gmail.com>
2023-06-19 23:40:49 +0530

    updated docker-file

--- frontend/senderSocket.js
+++ frontend/senderSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://mq-visualizer.site/socker.io/';
+const URL = 'https://mq-visualizer.site';
 
 export const senderSocket = io(URL);
