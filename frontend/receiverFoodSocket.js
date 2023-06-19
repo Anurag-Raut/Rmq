78cd67b6918aec7da2d68b0d0fce1bc6be3d5def
Anurag-Raut <rautab0@gmail.com>
2023-06-19 14:40:47 +0530

    url update

--- frontend/receiverFoodSocket.js
+++ frontend/receiverFoodSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:4000';
+const URL = 'http://157.245.109.88:4000';
 
 export const recieverFoodSocket = io(URL);
