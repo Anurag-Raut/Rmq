8ecba311c08e0b896f36dd327a4ab0e27c6481b8
Anurag-Raut <rautab0@gmail.com>
2023-06-19 23:26:29 +0530

    updated docker-file

--- frontend/receiverFoodSocket.js
+++ frontend/receiverFoodSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://mq-visualizer.site/';
+const URL = 'https://mq-visualizer.site/a';
 
 export const recieverFoodSocket = io(URL);
