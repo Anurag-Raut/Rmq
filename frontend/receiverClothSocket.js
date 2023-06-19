7ee2c13425ec9ae82fd6d92ff973ae9d99d8b7b6
Anurag-Raut <rautab0@gmail.com>
2023-06-19 19:40:28 +0530

    url

--- frontend/receiverClothSocket.js
+++ frontend/receiverClothSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://157.245.109:5000';
+const URL = 'https://mq-visualizer.site/receivercloth';
 
 export const recieverClothSocket = io(URL);
