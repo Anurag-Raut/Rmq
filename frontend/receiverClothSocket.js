4e4e562e1b1b29bdd3ae7c67a77171a0698f51a6
Anurag-Raut <rautab0@gmail.com>
2023-06-19 23:31:27 +0530

    updated docker-file

--- frontend/receiverClothSocket.js
+++ frontend/receiverClothSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://mq-visualizer.site';
+const URL = 'https://mq-visualizer.site/b';
 
 export const recieverClothSocket = io(URL);
