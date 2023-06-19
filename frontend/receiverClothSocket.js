9cefd5674bdad3ad0bfcc573b40e586a6517b44f
Anurag-Raut <rautab0@gmail.com>
2023-06-19 18:53:20 +0530

    changed socket urls

--- frontend/receiverClothSocket.js
+++ frontend/receiverClothSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://mq-visualizer.site/receivercloth';
+const URL = 'https://157.245.109:5000';
 
 export const recieverClothSocket = io(URL);
--- frontend/senderSocket.js
+++ frontend/senderSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://mq-visualizer.site/sender';
+const URL = 'https://157.245.109:3000';
 
 export const senderSocket = io(URL);
