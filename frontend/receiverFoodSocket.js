1e4bc3585a9959b84e559fa92ebabc8274bec9bc
Anurag-Raut <rautab0@gmail.com>
2023-06-19 17:44:31 +0530

    changed socket urls

--- frontend/receiverFoodSocket.js
+++ frontend/receiverFoodSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'https://mq-visualizer.site/receiverfood';
+const URL = 'https://mq-visualizer.site/receiverfood'
 
 export const recieverFoodSocket = io(URL);
