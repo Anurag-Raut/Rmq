398f68bac62b83191d697ab9fc9aaf5f60be96d3
Anurag-Raut <rautab0@gmail.com>
2023-06-19 17:25:03 +0530

    changed socket urls

--- frontend/senderSocket.js
+++ frontend/senderSocket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://157.245.109.88:3000';
+const URL = 'https://mq-visualizer.site/sender';
 
 export const senderSocket = io(URL);
