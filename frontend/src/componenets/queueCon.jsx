bf53dd5a65a0156b5529221dff2cbbbc8b4632c9
Anurag-Raut <rautab0@gmail.com>
2023-06-20 11:25:15 +0530

    url

--- frontend/src/componenets/queueCon.jsx
+++ frontend/src/componenets/queueCon.jsx
@@ -3,14 +3,14 @@ function QueueCon({ queue }) {
   return (
     <div>
       {queue?.map((data, index) => {
-        console.log(data.time)
+        console.log(data.time- new Date().getTime())
         return (
           <div key={index} class="flex justify-between w-3">
             <h4 className="mr-3" key={index}>
               {data.orderId}
             </h4>
             <Countdown
-              date={data.time-new Date().getTime()}
+              date={data.time- new Date().getTime()}
               intervalDelay={0}
               precision={1}
               renderer={(props) => <div>{props.total / 1000}</div>}
